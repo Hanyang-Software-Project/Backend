4b392aecd367ec421f23c800d5a5708f26edf510
Ivan Milos <81751805+ivanmilos19@users.noreply.github.com>
2024-10-31 21:27:39 +0900

    initial commit

---
+++ src/test/java/com/ziggs/ziggs_backend/ZiggsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ziggs.ziggs_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZiggsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
