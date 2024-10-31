fe19a162956e89a78825f08569edd6e085f684c9
Ivan Milos <81751805+ivanmilos19@users.noreply.github.com>
2024-10-31 21:27:39 +0900

    initial commit

---
+++ src/main/java/com/ziggs/ziggs_backend/ZiggsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ziggs.ziggs_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZiggsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZiggsBackendApplication.class, args);
+    }
+
+}
