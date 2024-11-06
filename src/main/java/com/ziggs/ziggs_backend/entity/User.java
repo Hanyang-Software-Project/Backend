92f68c021fc48d7b99b10fa5d18717658d79c1fb
Ivan Milos <81751805+ivanmilos19@users.noreply.github.com>
2024-11-06 23:36:13 +0900

    adjustments to the User entity for created_at and added role

--- src/main/java/com/ziggs/ziggs_backend/entity/User.java
+++ src/main/java/com/ziggs/ziggs_backend/entity/User.java
@@ -17,6 +17,9 @@ public class User {
     @Column(name = "user_id")
     private Long userId;
 
+    @Column(name = "username", length = 50)
+    private String username;
+
     @Email
     @Column(name = "email", nullable = false, length = 255)
     private String email;
@@ -24,20 +27,22 @@ public class User {
     @Column(name = "password_hash", nullable = false, length = 255)
     private String passwordHash;
 
-    @Column(name = "username", length = 50)
-    private String username;
-
-    @Column(name = "last_name", length = 50)
-    private String lastName;
-
     @Column(name = "created_at")
     @Temporal(TemporalType.TIMESTAMP)
     private Date createdAt;
+
+    @Column(name = "role", nullable = false, length = 50)
+    private String role;
 
     @OneToMany(mappedBy = "user")
     private List<FeedbackTicket> feedbackTickets;
 
     @OneToMany(mappedBy = "user")
     private List<Alert> alerts;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = new Date();
+    }
 
 }
