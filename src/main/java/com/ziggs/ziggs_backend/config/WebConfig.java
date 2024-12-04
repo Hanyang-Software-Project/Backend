23e3bb4b74fd9637548d4329125a0d128dbc4d5b
Wallblue <nrb.busquet@gmail.com>
2024-12-04 10:47:54 +0900

    Add all methods to CORS rules

--- src/main/java/com/ziggs/ziggs_backend/config/WebConfig.java
+++ src/main/java/com/ziggs/ziggs_backend/config/WebConfig.java
@@ -22,6 +22,7 @@ public class WebConfig implements WebMvcConfigurer {
     }
 
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
